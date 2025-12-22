5befd5ae0fdf6ebb31bf122621c615c7041a8592
Eugene_Crabs_The_Third <geka001998@gmail.com>
2025-12-22 21:47:07 +0300

    Add files via upload

---
+++ lesson4.cpp
@@ -0,0 +1,64 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    /*float L, P;
+    int i = 0;
+        std::cout << "input the dividend (length of fabric): \n";
+        std::cin >> L;
+        std::cout << "input the divider (amount of pillows): \n";
+        std::cin >> P;
+        for (; L >= P+P; i++, L -= P+P);
+        std::cout << i;*/
+
+    
+        
+        
+        
+   /*char Big = 65, small = 97;
+    while (Big != 90)
+    {
+        std::cout << Big << ", " << small << ", ";
+        Big++;
+        small++;
+    }
+    std::cout << Big << ", " << small << ".";*/
+
+
+
+    
+    /*for (int i = 1; i <= 100; i++)
+        if (i % 10 == 0)
+            std::cout << i << std::endl;
+        else
+            std::cout << i << " ";*/
+
+   /* for (int i = 0; i <= 9; i++)
+    {
+        for (int j = 1; j < 10; j++)
+            std::cout << i << j << " ";
+        std::cout << i+1 << 0<< std::endl;
+    }*/
+
+    /*for (int i = 0, c = 1; i < 10; i++, c++)
+    {
+        if (c % 2 == 0)
+        {
+            for (int j = 0; j < 10; j++)
+                std::cout << "* ";
+        }
+        else
+        {
+            for (int j = 0; j < 10; j++)
+                std::cout << ". ";
+
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;*/
+
+
+}
+
