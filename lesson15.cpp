5f30c5d5e820cd20d6738e0601f2c6de52620906
Eugene_Crabs_The_Third <geka001998@gmail.com>
2026-01-30 21:49:41 +0300

    Add files via upload

---
+++ lesson15.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <iostream>
+
+
+int value_reverse(int value);
+
+void sum(int a, int b, int *s);
+
+
+int main()
+{
+    //std::cout << value_reverse(1467);
+
+    int a = 5;
+    int b = 6;
+
+    int s = 0;
+
+    sum(a, b, &s);
+
+    std::cout << s;
+}
+
+
+
+int value_reverse(int value) {
+
+    int v = value % 10;
+    if (v == 0)
+        return v;
+    
+   int result = value_reverse(value / 10) * 10 + v;
+    
+    return result;
+}
+
+// i have failed.
+
+
+
+void sum(int a, int b, int *s)
+{
+    *s = a + b;
+}
