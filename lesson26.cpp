d563f9a3bafd6a1b74645d4c193c78a50d28e9f0
Eugene_Crabs_The_Third <geka001998@gmail.com>
2026-02-27 21:48:01 +0300

    Add files via upload

---
+++ lesson26.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+struct Person{
+    static const int LEN = 100;
+    char name[LEN];
+    int age{};
+
+};
+
+int main()
+{
+    Person arr[10]{ {"Andray", 65 },
+                    {"Paul", 73 },
+                    {"Ann", 3 },
+                    {"Dmitry", 88 },
+                    {"Anastatia", 10 },
+                    {"Olga", 25 },
+                    {"Vladimir", 34 },
+                    {"Eugene", 42 },
+                    {"Katya", 8 },
+                    {"Eva", 12 } };
+
+
+    char path[] = "PersonData.txt";
+    FILE* db;
+    
+            //write
+    if (fopen_s(&db, path, "wb") != 0) {
+        std::cout << "ERROR";
+    }
+    else {
+        fwrite(arr, sizeof(arr[0]), 10, db);
+        fclose(db);
+    }
+
+            //read
+    if (fopen_s(&db, path, "rb") != 0) {
+        std::cout << "ERROR";
+    }
+    else {
+        Person buff[10]{};
+        fread(buff, sizeof(buff[0]), 10, db);
+            for (int i = 0; i < 10; i++)
+                std::cout << buff[i].name << " " << buff[i].age << "\n";
+        fclose(db);
+    }
+}
+
