8aaa0224cabdd0bb1e56050cacc896b9c7e446a5
Eugene_Crabs_The_Third <geka001998@gmail.com>
2026-04-15 21:44:42 +0300

    Add files via upload

---
+++ OOP13.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+
+
+int main()
+{
+    std::string buf;
+    std::ifstream file("source.txt");
+    if (!file.is_open()) {
+        std::cerr << "Open erorr" << std::endl;
+        return 1;
+    }
+    std::getline(file, buf);
+    file.close();
+
+    for (int i = 0; buf[i] != 0; i++) {
+        if (buf[i] == ' ') {
+            buf[i] = '_';
+        }
+
+    }
+
+    std::ofstream fin("dest.txt");
+    if (!fin.is_open()) {
+        std::cerr << "open Error" << std::endl;
+        return 1;
+    }
+    fin << buf;
+    fin.close();
+}
+
